26140270937e02d74993dedacad7c5e2bee2a3e6
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-09-08 02:12:49 -0500

    Se abre el paquete modelo a javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module org.chinosoft {
 
     opens org.chinosoft to javafx.fxml, javafx.controls;
     opens org.chinosoft.controlador to javafx.fxml;
+    opens org.chinosoft.modelo to javafx.base;
     exports org.chinosoft;
     exports org.chinosoft.controlador;
 }
